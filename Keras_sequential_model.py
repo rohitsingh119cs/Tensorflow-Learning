f40d8a434d39f5805674e80a192c7ddd10eceb87
Rohit Singh <rohitsingh119cs@gmail.com>
2020-07-06 01:03:30 +0530

    Updated File

--- Keras_sequential_model.py
+++ Keras_sequential_model.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
 
-# Helper libraries
-
 print('TensorFlow version: {}'.format(tf.__version__))
+
+# ######################################## Prepare Datasets ###########################################################
 
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+print(train_labels)
 
 # scale the values to 0.0 to 1.0
 train_images = train_images / 255.0
@@ -16,33 +17,44 @@ test_images = test_images / 255.0
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
 test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
+
+# Total number of classes and their names used for training the sequential model
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+number_of_classes = 10
 
-print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
-print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+print('Train_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
+print('Test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
 
+# ########################### Creating Sequential Layers of CNN #######################################################
 model = keras.Sequential([
     keras.layers.Conv2D(input_shape=(28, 28, 1), filters=8, kernel_size=3, strides=2, activation='relu', name='Conv1'),
     keras.layers.Flatten(),
-    keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
+    keras.layers.Dense(number_of_classes, activation=tf.nn.softmax, name='Softmax')
 ])
-print("Summary of the Model", model.summary())
 
+print("Summary of the Model")
+model.summary()
+
+print("Compiling Model")
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=2)
+print("Compiling Model Done")
 
+print("Starting Model Training")
+model.fit(train_images, train_labels, epochs=10)
+print("Model Training Done")
+
+print("Evaluating Model Accuracy:")
 test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('\nTest accuracy: {}'.format(test_acc))
-print('\nTest loss: {}'.format(test_loss))
+print('Test accuracy: {}'.format(test_acc))
+print('Test loss: {}'.format(test_loss))
+
 # Saving Model to Directory
 # Create .PB file which is proto buffer
-
-MODEL_DIR = "model/"
-version = 1
-export_path = MODEL_DIR
+print('\nSaving Model To Directory path')
+export_path = "model/"
 print('export_path = {}\n'.format(export_path))
 
-output = tf.keras.models.save_model(
+tf.keras.models.save_model(
     model,
     export_path,
     overwrite=True,
@@ -52,4 +64,4 @@ output = tf.keras.models.save_model(
     options=None
 )
 
-print('\nSaved model:', output)
+print('\nModel Saved To:', export_path)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Tensorflow-Learning
- 
+# Machine Learning
+### Supervised Learning Algorithms
+- Support vector machine
+- Neural network
+- Linear and Logistics regression
+- Random forest
+- Classification trees
+
+### Unsupervised Learning Algorithms
+- Cluster algorithms,
+- K-means
+- Hierarchical clustering
+
